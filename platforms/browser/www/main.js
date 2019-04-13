0101439fe129fb8f2a95f38860e90dd1fcff08d0
jadasi <maxy.n.zeke@gmail.com>
2019-04-12 17:35:29 -0700

    First

---
+++ platforms/browser/www/main.js
@@ -0,0 +1,18 @@
+var app = new Framework7({
+    root: '#app',
+    name: 'My App',
+    id: 'com.myapp.test',
+    routes: [
+        {
+            path: '/login/',
+            url: '../login.html',
+    },
+        {
+            path: '/signup/',
+            url: '../signup.html',
+    },
+
+  ],
+});
+
+var mainView = app.views.create('.view-main');
