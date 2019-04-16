e9dbe7c7372c0dc4be1a1a96487f6baf40bc1e27
jadasi <maxy.n.zeke@gmail.com>
2019-04-16 14:53:29 -0700

    Setup Main UI

--- platforms/browser/www/js/chat.js
+++ platforms/browser/www/js/chat.js
@@ -1,26 +1,3 @@
-var $$ = Dom7;
-
-var app = new Framework7({
-    root: '#app',
-    name: 'My App',
-    id: 'com.myapp.test',
-    routes: [
-        /*
-        {
-            path: '/login/',
-            url: 'login.html',
-    },
-        {
-            path: '/signup/',
-            url: 'signup.html',
-    },
-*/
-  ],
-});
-
-var mainView = app.views.create('.view-main');
-
-
 // Init Messages
 var messages = app.messages.create({
   el: '.messages',
