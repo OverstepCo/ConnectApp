106fcc955442d656af1dc88ab9c1f5fe6d2ca9bc
David Silva <maxy.n.zeke@gmail.com>
2019-04-17 17:44:53 -0700

    added to panel and started login screen

--- www/js/main.js
+++ www/js/main.js
@@ -4,29 +4,43 @@ var app = new Framework7({
     root: '#app',
     name: 'My App',
     id: 'com.myapp.test',
+    panel: {
+        swipe: 'both',
+        swipeActiveArea: 30,
+    },
+    navbar: {
+        hideOnPageScroll: true,
+    },
     routes: [
-        /*
+ // Index page
         {
-            path: '/login/',
-            url: 'login.html',
-    },
+            path: '/',
+            url: './index.html',
+            name: 'home',
+  },
+  // Chat page
         {
-            path: '/signup/',
-            url: 'signup.html',
-    },
-*/
+            path: '/chat/',
+            url: '../pages/chat.html',
+            name: 'chat',
+  },
+        // login page
+        {
+            path: '/login-screen/',
+            url: 'pages/login.html',
+  },
   ],
 });
 
 var mainView = app.views.create('.view-main');
 
 var loginScreen = app.loginScreen.create({
-  content: '<div class="login-screen">...</div>',
-  on: {
-    opened: function () {
-      console.log('Login Screen opened')
+    content: '<div class="login-screen">...</div>',
+    on: {
+        opened: function () {
+            console.log('Login Screen opened')
+        }
     }
-  }
 })
 
 //loginScreen.open();
