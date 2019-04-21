3ce652beb5ec902fedb0c489c100a9478113a2dd
David Silva <maxy.n.zeke@gmail.com>
2019-04-20 23:00:41 -0700

    Large UI Overhaul

--- www/js/main.js
+++ www/js/main.js
@@ -1,49 +1,45 @@
 var $$ = Dom7;
 
 var app = new Framework7({
-    root: '#app',
-    name: 'My App',
-    id: 'com.myapp.test',
-    panel: {
-        swipe: 'both',
-        swipeOnlyClose: true,
-        swipeActiveArea: 30,
+  root: '#app',
+  name: 'My App',
+  id: 'com.myapp.test',
+  panel: {
+    swipe: 'both',
+    swipeOnlyClose: true,
+    swipeActiveArea: 30,
+  },
+  navbar: {
+    hideOnPageScroll: false,
+  },
+  routes: [
+    // Index page
+    {
+      path: '/home/',
+      url: 'index.html',
     },
-    navbar: {
-        hideOnPageScroll: false,
+    // signup page
+    {
+      path: '/signup-screen/',
+      url: 'pages/signup.html',
     },
-    routes: [
-        // Index page
-        {
-            path: '/home/',
-            url: 'index.html',
-  },
-        // signup page
-        {
-            path: '/signup-screen/',
-            url: 'pages/signup.html',
-  },
-        // login page
-        {
-            path: '/login-screen/',
-            url: 'pages/login.html',
-  },
-         {
-            path: '/chat-screen/',
-            url: 'pages/chat.html',
-  },
+    // login page
+    {
+      path: '/login-screen/',
+      url: 'pages/login.html',
+    },
+    {
+      path: '/chat-screen/',
+      url: 'pages/chat.html',
+    },
   ],
 });
 
 var mainView = app.views.create('.view-main');
 
-var loginScreen = app.loginScreen.create({
-    content: '<div class="login-screen">...</div>',
-    on: {
-        opened: function () {
-            console.log('Login Screen opened')
-        }
-    }
-})
-
-//loginScreen.open();
+// create searchbar
+var searchbar = app.searchbar.create({
+  el: '.searchbar',
+  searchContainer: '.members-list',
+  searchIn: '.item-inner',
+});
