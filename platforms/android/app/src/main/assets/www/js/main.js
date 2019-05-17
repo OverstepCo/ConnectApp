1954417e775a1cb3238f591cc2c7e5792f8ef1ff
jadasi <maxy.n.zeke@gmail.com>
2019-05-17 15:00:53 -0700

    removed browser suppport & added cordova firebase plugin

--- platforms/android/app/src/main/assets/www/js/main.js
+++ platforms/android/app/src/main/assets/www/js/main.js
@@ -1,49 +1,118 @@
 var $$ = Dom7;
 
 var app = new Framework7({
-    root: '#app',
-    name: 'My App',
-    id: 'com.myapp.test',
-    panel: {
-        swipe: 'both',
-        swipeOnlyClose: true,
-        swipeActiveArea: 30,
+  root: '#app',
+  name: 'My App',
+  id: 'com.myapp.test',
+  theme: 'md',
+  panel: {
+    swipe: 'both',
+    swipeOnlyClose: true,
+    swipeActiveArea: 30,
+  },
+  navbar: {
+    hideOnPageScroll: false,
+  },
+  toolbar: {
+    showOnPageScrollEnd: true,
+  },
+  routes: [
+    // Index page
+    {
+      path: '/home/',
+      url: 'index.html',
     },
-    navbar: {
-        hideOnPageScroll: false,
+    // profile page
+    {
+      path: '/profile-screen/',
+      url: 'pages/profile.html',
     },
-    routes: [
-        // Index page
-        {
-            path: '/home/',
-            url: 'index.html',
-  },
-        // signup page
-        {
-            path: '/signup-screen/',
-            url: 'pages/signup.html',
-  },
-        // login page
-        {
-            path: '/login-screen/',
-            url: 'pages/login.html',
-  },
-         {
-            path: '/chat-screen/',
-            url: 'pages/chat.html',
-  },
+    // signup page
+    {
+      path: '/signup-screen/',
+      url: 'pages/signup.html',
+    },
+    // login page
+    {
+      path: '/login-screen/',
+      url: 'pages/login.html',
+    },
+    // chat page
+
+    {
+      path: '/chat-screen/',
+      url: 'pages/chat.html',
+      on: {
+        pageAfterIn: function test(e, page) {
+          // do something after page gets into the view
+        },
+        pageInit: function(e, page) {
+          // do something when page initialized
+          setupChat();
+
+        },
+        pageBeforeRemove: function(e, page) {
+          console.log('page before remove');
+          listener();
+          app.messages.destroy('.messages');
+        },
+      }
+    },
+    // preview chat page
+    {
+      path: '/preview-chat-screen/',
+      url: 'pages/preview_chat.html',
+    },
+    // settings page
+    {
+      path: '/settings-screen/',
+      url: 'pages/settings.html',
+    },
+    // about page
+    {
+      path: '/about-screen/',
+      url: 'pages/about.html',
+    },
+    // edit profile page
+    {
+      path: '/edit-profile-screen/',
+      url: 'pages/edit_profile.html',
+    },
+    // edit new event page
+    {
+      path: '/new-event-screen/',
+      url: 'pages/new_event.html',
+    },
+    // edit new chat page
+    {
+      path: '/new-chat-screen/',
+      url: 'pages/new_chat.html',
+    },
   ],
 });
 
+
 var mainView = app.views.create('.view-main');
+var swiper;
 
-var loginScreen = app.loginScreen.create({
-    content: '<div class="login-screen">...</div>',
-    on: {
-        opened: function () {
-            console.log('Login Screen opened')
-        }
-    }
-})
+//get event swiper
 
-//loginScreen.open();
+// create searchbar
+var searchbar = app.searchbar.create({
+  el: '.searchbar',
+  searchContainer: '.members-list',
+  searchIn: '.item-inner',
+});
+
+//next/prev event card
+function swipeRight() {
+  swiper.slideNext();
+}
+
+function swipeLeft() {
+  swiper.slidePrev();
+}
+
+function updateSwiper() {
+  swiper = app.swiper.get('.swiper-container');
+}
